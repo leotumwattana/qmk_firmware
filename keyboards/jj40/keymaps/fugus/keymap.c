25cd71460fdb5433fe7e767b768b09384e3fe6e8
Leo Tumwattana <leo.tumwattana@gmail.com>
2018-11-16 04:14:45 +0800

    more tweaks

--- keyboards/jj40/keymaps/fugus/keymap.c
+++ keyboards/jj40/keymaps/fugus/keymap.c
@@ -29,19 +29,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  LT(_NAV, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  LT(_NAV, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
-  TT(_NUMPAD), KC_LCTL, KC_LALT, LGUI_T(KC_BSPC), LT(_LOWER, KC_ESC),  KC_SPC, KC_SPC,  LT(_RAISE, KC_ENT),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  TT(_NUMPAD), KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_ESC),  KC_SPC, KC_SPC,  LT(_RAISE, KC_ENT),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |      |      |  (   |  )   |      |      |  (   |   )  |      |      |      |
+ * |   ~  |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |      |      |  {   |  }   |      |      | [{   |   ]} |      |      |      |
+ * | Del  |      |      |      |      |      |      |  {   |   }  |  (   |   )  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |  [   |  ]   |      |      |  {   |   }  |      |      |      |
+ * |      |      |      |      |      |      |      |  [   |   ]  |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_TILD, _______, _______, KC_LPRN, KC_RPRN, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, \
-  KC_DEL, _______, _______, KC_LCBR, KC_RCBR, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, \
-  _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, \
+  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
+  KC_DEL, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQUAL, KC_BSLS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_PIPE, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | DEL  | Bksp |      |      |Vol - |Vol + | Mute |
+ * |      |      |      |      |      |Enter |Enter |      |      |Vol - |Vol + | Mute |
  * `-----------------------------------------------------------------------------------'
  */
 
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_TAB, KC_ENT, _______, _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, KC_MUTE, \
   _______, _______, KC_LGUI, KC_LALT, KC_LSFT, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_VOLU, \
   _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, KC_VOLD, \
-  _______, _______, _______, _______, _______, KC_DEL, KC_BSPC, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R  \
+  _______, _______, _______, _______, _______, KC_ENT, KC_ENT, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R  \
 ),
 
 /* NUMPAD
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RGB_TOG, RGB_HUD, RGB_HUI, _______, _______, _______, KC_LPRN, KC_7, KC_8, KC_9, KC_PLUS, KC_BSPC, \
   RGB_MOD, RGB_SAD, RGB_SAI, _______, _______, _______, KC_RPRN, KC_4, KC_5, KC_6, KC_MINS, KC_ENT, \
   _______, RGB_VAD, RGB_VAI, _______, _______, _______, KC_CIRC , KC_1, KC_2, KC_3, KC_ASTR, KC_COMM, \
-  _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_G, KC_WAKE, KC_WAKE, KC_0, KC_DOT, KC_EQL, KC_SLSH, KC_SLEP  \
+  _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_G, KC_SPC, KC_SPC, KC_0, KC_DOT, KC_EQL, KC_SLSH, _______ \
 )
 };
 
