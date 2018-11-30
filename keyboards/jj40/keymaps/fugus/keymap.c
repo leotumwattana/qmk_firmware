37345675a98dabd44b81f9633c32fa5574ee3a2d
Leo Tumwattana <leo.tumwattana@gmail.com>
2018-11-30 14:57:38 +0800

    Updates keymap for jj40

--- keyboards/jj40/keymaps/fugus/keymap.c
+++ keyboards/jj40/keymaps/fugus/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LSFT_T(KC_ESC),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LSFT_T(KC_BSPC), \
   LT(_NAV, KC_TAB),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
-  TT(_NUMPAD), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER),  LSFT_T(KC_SPC), LSFT_T(KC_SPC), MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  TT(_NUMPAD), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER),  OSM(MOD_LSFT), LSFT_T(KC_SPC), MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
   /* TT(_NUMPAD), KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER),  KC_SPC, KC_SPC, MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \ */
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   -  |   =  |   \  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   -  |   +  |   _  |   |  |      |
+ * |      |   +  |   -  |   *  |   /  |      |      |   -  |   +  |   _  |   |  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |Enter |   =  |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
   _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQUAL, KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_PLUS, KC_UNDS, KC_PIPE, _______, \
+  _______, KC_PLUS, KC_MINS, KC_ASTR, _______, _______, _______, KC_MINS, KC_PLUS, KC_UNDS, KC_PIPE, _______, \
   _______, _______, _______, _______, KC_ENT, KC_EQUAL, _______, _______, _______, _______, _______, _______  \
 ),
 
