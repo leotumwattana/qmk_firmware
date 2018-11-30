c3058740dc92bc4f0836554172ae9b0e9cc5c2a8
Leo Tumwattana <leo.tumwattana@gmail.com>
2018-11-30 14:57:10 +0800

    Adds keymap for levinson

---
+++ keyboards/levinson/keymaps/kulio/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#define TAPPING_TERM 200
+
+#define MOUSEKEY_DELAY             200
+#define MOUSEKEY_INTERVAL          30
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       40
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
+#endif
