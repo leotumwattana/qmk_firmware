0fc1442309f209e45462d0b0df2c306cb09523ce
Leo Tumwattana <leo.tumwattana@gmail.com>
2018-12-24 06:37:59 +0800

    Adds PERMISSION_HOLD

--- keyboards/crkbd/keymaps/kulio/config.h
+++ keyboards/crkbd/keymaps/kulio/config.h
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 150
 #define TAPPING_TOGGLE 2
+#define PERMISSIVE_HOLD
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
